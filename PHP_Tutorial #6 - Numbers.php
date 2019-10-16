2b66a4478a826947a96ef8420083d26f354b17b3
xiaofei <xiaofeideyouxiang@gmail.com>
2019-10-16 21:58:42 +0200

    added code #6 Numbers finish

--- PHP_Tutorial #6 - Numbers.php
+++ PHP_Tutorial #6 - Numbers.php
@@ -2,8 +2,40 @@
 
     $radius = 25;
     $pi = 3.14;
+    // +, -, *, /, ^
+    // ** 幂^
+    // echo $pi* $radius ** 2;
 
-    echo $radius * $pi;
+    // 执行顺序
+    // echo 5 * (3 + 4) / 8;
+
+    // ++ --
+    // echo $radius++; // 25
+    // echo $radius;   // 26
+    // echo $radius--; // 25
+    // echo $radius;   // 24
+
+    // shorthand operators += -=
+    $age = 20;
+
+    // $age = $age + 10;
+    $age += 10;
+    echo $age;
+
+    // $age = $age - 10;
+    $age -= 10;
+    echo $age;
+
+    // number functions
+    // 向下取整
+    echo floor($pi);
+
+    // 向上取整
+    echo ceil($pi);
+
+    // pi
+    echo pi();
+
 
 ?>
 <!DOCTYPE html>
