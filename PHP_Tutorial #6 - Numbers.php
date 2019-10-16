70ff47b35c8087752cc32702f8bcf5c77722733b
xiaofei <xiaofeideyouxiang@gmail.com>
2019-10-16 21:42:59 +0200

    added #6 Numbers

---
+++ PHP_Tutorial #6 - Numbers.php
@@ -0,0 +1,20 @@
+<?php
+
+    $radius = 25;
+    $pi = 3.14;
+
+    echo $radius * $pi;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP_Tutorial #6 - Numbers</title>
+</head>
+<body>
+    
+</body>
+</html>
